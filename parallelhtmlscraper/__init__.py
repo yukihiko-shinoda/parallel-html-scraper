0081b78a013816d60bcf46c44b188482108a9610
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-08-08 20:50:00 +0900

    Introduce bump2version

--- parallelhtmlscraper/__init__.py
+++ parallelhtmlscraper/__init__.py
@@ -5,6 +5,8 @@ from parallelhtmlscraper.exceptions import *  # noqa
 from parallelhtmlscraper.html_analyzer import *  # noqa
 from parallelhtmlscraper.parallel_html_scraper import *  # noqa
 
+__version__ = "0.0.0"
+
 __all__: List[str] = []
 # pylint: disable=undefined-variable
 __all__ += exceptions.__all__  # type: ignore # noqa
